ea3ff75f8b0008ae6f93d52858623c94e2ab04ed
zhh419 <zhanghh@knowbox.cn>
2019-09-25 20:20:33 +0800

    update teacher.sql

--- sql/teacher.sql
+++ sql/teacher.sql
@@ -1,118 +1,76 @@
-select
-teacher_id,
-source,
-subject,
-county_id,
-complete_novice_task,
-fst_hwk_type,
-cross_subject,
-is_be_invited,
-fst_month,
-case when min is null then 0 else 1 end as status,
-case when min is null then max else min end as s_time
+select eee.teacher_id,
+       ot.source, ot.subject, ot.county_id,
+       ts.fst_hwk_type, ts.complete_novice_task, ts.is_be_invited, ts.cross_subject,
+       eee.fst_day, eee.days, eee.status
 from
-(select
-teacher_id,
-source,
-subject,
-county_id,
-complete_novice_task,
-fst_hwk_type,
-cross_subject,
-is_be_invited,
-fst_month,
-min (case when active_month is null then tem_number end),
-max(tem_number) 
-from
-(select
-ot.teacher_id,
-oh.fst_month,
-ot.source,
-ot.subject,
-ot.county_id,
-ts.complete_novice_task,
-ts.fst_hwk_type,
-ts.cross_subject,
-ts.is_be_invited,
-otb.month as tem,
-ohb.active_month,
-row_number() over(partition by ot.teacher_id order by otb.month) -1 as tem_number
-from
--- 教师注册表
-(select
-teacher_id,
-case when source = 'androidRCTeacher' then 'Android'
-     when source = 'iPhoneRCTeacher' then 'iOS'
-     else 'Else' end as source,
-case when subject = 0 then 'Math'
-     when subject = 1 then 'Chi'
-     when subject = 2 then 'Eng'
-     else 'Sci' end as subject,
-county_id
-from view_o_teacher
-where student_count >= 20) ot
+    (select *,
+           max(case when flag = 1 then rn2 end) over (partition by teacher_id) flag4,
+           min(case when flag3 = 1 then rn2 end) over (partition by teacher_id) flag5
+    from
+        (select *,
+               case when max(flag3) over (partition by teacher_id) = 1 then 1 else 0 end as status--流失事件是否发生
+        from
+            (select *,
+                   count(1) over (partition by teacher_id, flag, flag1) flag2,--连续作业或连续无作业天数
+                   case when count(1) over (partition by teacher_id, flag, flag1) >= 90 and flag = 0 then 1 else 0 end as flag3--是否连续90天无作业
+            from
+                (select teacher_id, fst_day, day, hday, flag, day - fst_day days,
+                       row_number() over (partition by teacher_id order by teacher_id, day) rn1,
+                       row_number() over (partition by teacher_id, flag order by teacher_id, day) rn2,
+                       row_number() over (partition by teacher_id order by teacher_id, day) - row_number() over (partition by teacher_id,flag order by teacher_id, day) flag1
+                from
+                    (select fst.teacher_id, fst.fst_day, m1.day, oh1.day hday,
+                            case when oh1.day is null then 0 else 1 end flag--当天是否作业活跃
+                    from
+                        (select oh.teacher_id, min(oh.day) fst_day
+                        from o_homework oh
+                        join view_o_teacher ot on ot.teacher_id = oh.teacher_id and ot.student_count >= 20
+                        group by 1)fst--首次作业活跃时间
 
+                    join
+                        (select day, month
+                        from view_o_student
+                        where month not in ('2015-02-01', '2015-07-01', '2015-08-01',
+                                            '2016-02-01', '2016-07-01', '2016-08-01',
+                                            '2017-02-01', '2017-07-01', '2017-08-01',
+                                            '2018-02-01', '2018-07-01', '2018-08-01',
+                                            '2019-02-01', '2019-07-01', '2019-08-01')
+                        group by 1,2)m1 on m1.day >= fst.fst_day--拓展出每个人首次作业时间至今每一天
+
+                    left join
+                        (select teacher_id, day
+                        from o_homework
+                        group by 1,2)oh1 on fst.teacher_id = oh1.teacher_id and m1.day = oh1.day--每个人所有作业日期（去重）
+
+                    where fst.fst_day between '2015-01-01' and '2019-09-01'
+                    )aaa
+                )bbb
+            )ccc
+        )ddd
+    )eee
 
 join
--- 获取特征数据
-(select
-teacher_id,
-complete_novice_task,
-case when first_assgin_hwk_type_code in ('All', 'Review', 'OnlineMatch', 'Ocr', 'Listen&Write', 'ReadingExam', 'PersonalReading') then 'Daily' else 'Match' end as fst_hwk_type,
-cross_subject,
-is_be_invited
-from o_teacher_stat) ts
-on ts.teacher_id = ot.teacher_id
+    (select teacher_id,
+            case when source = 'androidRCTeacher' then 'Android'
+                 when source = 'iPhoneRCTeacher' then 'iOS'
+                 else 'Else' end as source,
+            case when subject = 0 then 'Math'
+                 when subject = 1 then 'Chi'
+                 when subject = 2 then 'Eng'
+                 else 'Sci' end as subject,
+            county_id
+    from view_o_teacher
+    where student_count >= 20
+    )ot on eee.teacher_id = ot.teacher_id-- 教师注册表
 
 join
--- 获取教师的出生日期
-(select
-teacher_id,
-min(month) as fst_month
-from o_homework
--- -- 限制首次活跃时间不为2019年9月
-where month < '2019-09-01'
-group by teacher_id) oh
+    (select teacher_id,
+            complete_novice_task,
+            case when first_assgin_hwk_type_code in ('All', 'Review', 'OnlineMatch', 'Ocr', 'Listen&Write', 'ReadingExam', 'PersonalReading') then 'Daily' else 'Match' end as fst_hwk_type,
+            cross_subject,
+            is_be_invited
+    from o_teacher_stat
+    ) ts on ts.teacher_id = ot.teacher_id-- 获取特征数据
 
-on ot.teacher_id = oh.teacher_id
-
-
-join
--- 中转日期
-(select
-month
-from view_o_teacher
--- 不含假期
-where month not in ('2015-02-01', '2015-07-01', '2015-08-01',
-		    '2016-02-01', '2016-07-01', '2016-08-01',
-		    '2017-02-01', '2017-07-01', '2017-08-01',
-		    '2018-02-01', '2018-07-01', '2018-08-01',
-		    '2019-02-01', '2019-07-01', '2019-08-01')
-group by 1) otb
-
-on 1 = 1 
-
-
-left join
--- 获取教师活动数据
-(select
-teacher_id,
-month as active_month
-from o_homework
-group by 1, 2) ohb
-
-on ot.teacher_id = ohb.teacher_id
-and ohb.active_month = otb.month
-
-where oh.fst_month <= otb.month
--- 剔除假期注册的老师
-and oh.fst_month not in ('2015-02-01', '2015-07-01', '2015-08-01',
-		         '2016-02-01', '2016-07-01', '2016-08-01',
-		         '2017-02-01', '2017-07-01', '2017-08-01',
-		         '2018-02-01', '2018-07-01', '2018-08-01',
-		         '2019-02-01', '2019-07-01', '2019-08-01')
-) aa
-
-group by 1, 2, 3, 4, 5, 6, 7, 8, 9) aa
-
-order by 1
+where (eee.status = 0 and eee.flag4 = eee.rn2 and eee.flag = 1) --事件未发生的人对应最后一条作业活跃数据
+   or (eee.status = 1 and eee.flag5 = eee.rn2 and eee.flag = 0); --事件发生的人对应第一条作业未活跃数据
